6488f5d33bac0f257fc0cd6eddb0e214bf775618
Dasom Heo <dasomee@outlook.com>
2023-04-26 22:51:27 -0400

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import image from "./images/icon-pro.svg";
+import "./style.scss";
+
+const myImage = document.createElement("img");
+myImage.src = image;
+document.body.appendChild(myImage);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+};
