88a4db672f14805ac6f224ce432dbb35460797f5
Dasom Heo <dasomee@outlook.com>
2023-05-03 22:28:58 -0400

    toggle switch styled

--- src/content_components/content/select-plan/generatePlanDurationToggle.js
+++ src/content_components/content/select-plan/generatePlanDurationToggle.js
@@ -1,21 +1,41 @@
 import PubSub from "pubsub-js";
 
 const generatePlanDurationToggle = () => {
-  const toggle = document.createElement("input");
-  toggle.classList.add("plan-duration-toggle");
-  toggle.setAttribute("type", "checkbox");
+  const label = document.createElement("label");
+  label.classList.add("switch");
+
+  const checkbox = document.createElement("input");
+  checkbox.classList.add("plan-duration-toggle");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.id = "toggle";
+
+  const slider = document.createElement("span");
+  slider.classList.add("slider");
+
+  label.appendChild(checkbox);
+  label.appendChild(slider);
 
   const storedPlanDuration = sessionStorage.getItem("plan-select.duration");
-  if (storedPlanDuration === "yearly") toggle.checked = true;
+  if (storedPlanDuration === "yearly") checkbox.checked = true;
 
-  const getSelectedPlanDuration = () => (toggle.checked ? "yearly" : "monthly");
+  const getSelectedPlanDuration = () =>
+    checkbox.checked ? "yearly" : "monthly";
 
-  toggle.addEventListener("change", () => {
+  const container = document.createElement("div");
+  container.classList.add("toggle-container");
+  const monthlyText = document.createTextNode("Monthly");
+  const yearlyText = document.createTextNode("Yearly");
+
+  checkbox.addEventListener("change", () => {
     sessionStorage.setItem("plan-select.duration", getSelectedPlanDuration());
     PubSub.publish("planDurationChanged");
   });
 
-  return toggle;
+  container.appendChild(monthlyText);
+  container.appendChild(label);
+  container.appendChild(yearlyText);
+
+  return container;
 };
 
 export { generatePlanDurationToggle };
